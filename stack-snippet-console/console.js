a348735668513c87dd7444564e81a05a7739c4f8
gh-canon <ryan@antisanity.net>
2015-05-07 10:03:19 -0400

    Limiting number of log entries.
    Added a configurable option for maxEntries accessible via
    console.config().

--- stack-snippet-console/console.js
+++ stack-snippet-console/console.js
@@ -3,9 +3,13 @@
 	if (!console) window.console = {};
 
 	var _log = console.log;
+	var _error = console.error;
+	var _warn = console.warn;
+	var _clear = console.clear;
 	var wrapper = document.createElement("div");
 	var div = document.createElement("div");
 	var style = document.createElement("style");
+	var maxEntries = 50;
 
 	wrapper.className = "as-console-wrapper";
 	div.className = "as-console";
@@ -14,19 +18,19 @@
 
 	style.type = "text/css";
 	style.textContent = [
-	".as-console-wrapper { position: fixed; bottom: 0; left: 0; right: 0; max-height: 120px; overflow-y: scroll; overflow-x: hidden; border-top: 1px solid #000000; }",
-	".as-console { background: #e9e9e9; border: 1px solid #ccc; display: table; width: 100%; counter-reset: console-row; border-collapse: collapse; }",
-	".as-console-row { display: table-row; counter-increment: console-row; font-family: monospace; font-size: 13px; }",
-	".as-console-row:before, .as-console-row:after { display: table-cell; padding: 3px 6px; content: counter(console-row); color: #bbb; border: 1px solid #ccc; }",
-	".as-console-row:after { content: attr(data-date); }",
-	".as-console-row + .as-console-row > * { border: 1px solid #ccc; }",
-	".as-console-row-code { width: 100%; white-space: pre-wrap; padding: 3px 5px; display: table-cell; font-family: monospace; font-size: 13px; }",
-	".as-console-error:before { content: 'Error: '; color: #f00  }",
-	"@-webkit-keyframes flash { 0% { background: rgba(255,240,0,.25); } 100% { background: none; } }",
-	"@-moz-keyframes flash { 0% { background: rgba(255,240,0,.25); } 100% { background: none; } }",
-	"@-ms-keyframes flash { 0% { background: rgba(255,240,0,.25); } 100% { background: none; } }",
-	"@keyframes flash { 0% { background: rgba(255,240,0,.25); } 100% { background: none; } }",
-	".as-console-row code, .as-console-row:before, .as-console-row:after { -webkit-animation: flash 1s; -moz-animation: flash 1s; -ms-animation: flash 1s; animation: flash 1s; }"].join("\n");
+    ".as-console-wrapper { position: fixed; bottom: 0; left: 0; right: 0; max-height: 150px; overflow-y: scroll; overflow-x: hidden; border-top: 1px solid #000; display: none; }",
+    ".as-console { background: #e9e9e9; border: 1px solid #ccc; display: table; width: 100%; counter-reset: console-row; border-collapse: collapse; }",
+    ".as-console-row { display: table-row; counter-increment: console-row; font-family: monospace; font-size: 13px; }",
+    ".as-console-row:after { display: table-cell; padding: 3px 6px; color: rgba(0,0,0,.35); border: 1px solid #ccc; content: attr(data-date); }",
+    ".as-console-row + .as-console-row > * { border: 1px solid #ccc; }",
+    ".as-console-row-code { width: 100%; white-space: pre-wrap; padding: 3px 5px; display: table-cell; font-family: monospace; font-size: 13px; }",
+    ".as-console-error:before { content: 'Error: '; color: #f00  }",
+    ".as-console-warning:before { content: 'Warning: '; color: #e90 }",
+    "@-webkit-keyframes flash { 0% { background: rgba(255,240,0,.25); } 100% { background: none; } }",
+    "@-moz-keyframes flash { 0% { background: rgba(255,240,0,.25); } 100% { background: none; } }",
+    "@-ms-keyframes flash { 0% { background: rgba(255,240,0,.25); } 100% { background: none; } }",
+    "@keyframes flash { 0% { background: rgba(255,240,0,.25); } 100% { background: none; } }",
+    ".as-console-row code, .as-console-row:before, .as-console-row:after { -webkit-animation: flash 1s; -moz-animation: flash 1s; -ms-animation: flash 1s; animation: flash 1s; }"].join("\n");
 
 	document.head.appendChild(style);
 
@@ -60,7 +64,7 @@
 		}
 	}
 
-	function formatDate(d) {		
+	function formatDate(d) {
 		d = new Date(d.valueOf() - d.getTimezoneOffset() * 60000);
 		return d.toISOString().replace("Z", "").replace("T", " ");
 	}
@@ -94,9 +98,15 @@
 		});
 	}
 
+	function truncateEntries() {
+		while (div.childNodes.length > maxEntries) {
+			div.removeChild(div.firstChild);
+		}
+	}
+
 	function createLogEntry() {
 
-		var args = arguments;
+		var args = arguments;		
 
 		var row = document.createElement("div");
 		row.className = "as-console-row";
@@ -114,9 +124,15 @@
 
 		div.appendChild(row);
 
+		truncateEntries();
+
 		wrapper.scrollTop = row.offsetTop;
 
 		return row;
+	}
+
+	function showConsole(show) {
+		wrapper.style.display = show ? "block" : "none";
 	}
 
 	console.log = function () {
@@ -127,7 +143,75 @@
 
 		createLogEntry.apply(null, args);
 
+		showConsole(1);
+
 	};
+
+	console.warn = function () {
+
+		var args = arguments;
+
+		_warn && _warn.apply(console, args);
+
+		createLogEntry.apply(null, args)
+            .children[0].classList.add("as-console-warning");
+
+		showConsole(1);
+
+	};
+
+	console.error = function () {
+
+		var args = arguments;
+
+		_error && _error.apply(console, args);
+
+		var entry;
+		var e = args[0];
+
+		if (e instanceof Error) {
+			entry = createLogEntry({
+				message: e.message,
+				filename: e.filename,
+				lineno: e.lineno,
+				colno: e.colno
+			});
+		} else {
+			entry = createLogEntry.apply(null, args)
+		}
+
+		entry.children[0].classList.add("as-console-error");
+
+		showConsole(1);
+
+	};
+
+	console.clear = function () {
+
+		while (div.lastChild) {
+			div.removeChild(div.lastChild);
+		}
+
+		_clear && _clear.apply(console, arguments);
+
+		showConsole(0);
+
+	};
+
+	console.config = function (settings) {
+
+		if (typeof settings === "object") {
+
+			if (settings.maxEntries > 0) {
+				maxEntries = settings.maxEntries;
+				truncateEntries();
+			}
+		}
+
+		console.log({
+			maxEntries: maxEntries
+		});
+	}
 
 	window.addEventListener("error", function (e) {
 		createLogEntry({
